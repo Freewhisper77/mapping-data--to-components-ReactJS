2217456475c8b72bcc335b2f5b9e58a9fbcc3dba
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 14:04:13 +0800

    feat: finish Task3. Map through the emojipedia array to render Entry components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
 //1.Create Entry component instead of [div className="term"]
 //2.Use props to replace hardcode data, then we will get 3 <Entry />
 //3.Map through the emojipedia array to render Entry components
 //3.1 Create a createEntry func
 //3.2 emojipedia.map(createEntry) to replace write <Entry /> 3 times
+
+function createEntry(info) {
+    return (
+        <Entry
+            key={info.id}
+            emoji={info.emoji}
+            name={info.name}
+            meaning={info.meaning}
+        />
+    );
+}
 
 function App() {
   return (
@@ -14,21 +26,7 @@ function App() {
         <span>emojipedia</span>
       </h1>
         <dl className="dictionary">
-            <Entry
-                emoji="💪"
-                name="Tense Biceps"
-                meaning="You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-            />
-            <Entry
-                emoji="🙏"
-                name="Person With Folded Hands"
-                meaning="Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-            />
-            <Entry
-                emoji="🤣"
-                name="Rolling On The Floor, Laughing"
-                meaning="This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-            />
+            {emojipedia.map(createEntry)}
         </dl>
     </div>
   );
