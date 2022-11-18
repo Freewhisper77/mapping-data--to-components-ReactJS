b64987da3a9d50dc7d01d9ad25e53c43e4c7360b
Freewhisper77 <emmasddx@foxmail.com>
2022-11-18 13:56:02 +0800

    feat: finish Task2. Use props to replace hardcode data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,21 @@ function App() {
         <span>emojipedia</span>
       </h1>
         <dl className="dictionary">
-            <Entry />
-            <Entry />
-            <Entry />
+            <Entry
+                emoji="💪"
+                name="Tense Biceps"
+                meaning="You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+            />
+            <Entry
+                emoji="🙏"
+                name="Person With Folded Hands"
+                meaning="Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+            />
+            <Entry
+                emoji="🤣"
+                name="Rolling On The Floor, Laughing"
+                meaning="This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+            />
         </dl>
     </div>
   );
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 
-function Entry() {
+function Entry(props) {
     return (
         <div className="term">
             <dt>
-                    <span className="emoji" role="img" aria-label="Tense Biceps">
-                      💪
-                    </span>
-                <span>Tense Biceps</span>
+                <span className="emoji" role="img" aria-label="Tense Biceps">
+                      {props.emoji}
+                </span>
+                <span>{props.name}</span>
             </dt>
-            <dd>
-                “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-                used in connection with doing sports, e.g. at the gym.
-            </dd>
+            <dd>{props.meaning}</dd>
         </div>
     );
 }
